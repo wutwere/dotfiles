0954355ad8d1876b7bcd1228fd5a34e2be0024d5
wutwere <blitesage@gmail.com>
2025-03-07 15:35:28 -0500

    better latex options

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -60,8 +60,11 @@ local PLUGINS = {
 		"lervag/vimtex",
 		lazy = false,
 		init = function()
+			vim.opt.conceallevel = 1
+			vim.g.tex_conceal = "abdmg"
 			vim.g.vimtex_view_method = "sioyek"
-			vim.g.vimtex_compiler_method = "latexmk"
+			vim.g.vimtex_callback_progpath = vim.fn.system("where nvim")
+			vim.g.vimtex_quickfix_open_on_warning = 0
 		end,
 	},
 }
