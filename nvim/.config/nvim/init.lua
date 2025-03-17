d313785f29ade6d3f412dc9f7e2c4de6b2a555ab
wutwere <blitesage@gmail.com>
2025-03-17 04:11:08 -0400

    how does this still work

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,6 +4,129 @@
 -- MAIN CONFIG --
 -----------------
 
+local KEYMAPS = {}
+do
+	vim.g.mapleader = " "
+	vim.g.maplocalleader = " "
+
+	KEYMAPS.general = function()
+		-- editor
+		vim.keymap.set("i", "{<cr>", "{<cr>}<esc>O")
+		vim.keymap.set("i", "{<s-cr>", "{<cr>}<esc>O")
+		vim.keymap.set("v", "<leader>y", '"+y', { desc = "Copy selected to clipboard" })
+		vim.keymap.set("n", "<leader>y", "<cmd>%y+<cr>", { desc = "Copy entire file contents to clipboard" })
+		vim.keymap.set("n", "<leader>Y", function()
+			vim.fn.setreg("+", vim.fn.expand("%:p"))
+		end, { desc = "Copy file path to clipboard" })
+		vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste clipboard" })
+		vim.keymap.set({ "n", "v" }, "<leader>P", '"+P', { desc = "Paste clipboard" })
+		vim.keymap.set("n", "<leader>m", "<cmd>e $MYVIMRC<cr>", { desc = "Edit vim config" })
+		vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show diagnostics at cursor" })
+		vim.keymap.set("n", "<leader>w", "<cmd>tabc<cr>", { desc = "Close tab" })
+
+		-- competitive programming
+		vim.keymap.set("n", "<leader>t", function()
+			vim.cmd("vsplit +term\\ ./run\\ " .. vim.fn.expand("%:r"))
+			vim.cmd("startinsert!")
+		end, { desc = "Run program in terminal split" })
+		vim.keymap.set("t", "<esc>", "<cmd>bd!<cr>")
+		vim.keymap.set("t", "<c-n>", "<c-\\><c-n>")
+
+		-- fast navigation
+		vim.keymap.set("n", "<leader>h", "<c-w>h", { desc = "Move to left pane" })
+		vim.keymap.set("n", "<leader>j", "<c-w>j", { desc = "Move to lower pane" })
+		vim.keymap.set("n", "<leader>k", "<c-w>k", { desc = "Move to upper pane" })
+		vim.keymap.set("n", "<leader>l", "<c-w>l", { desc = "Move to right pane" })
+		vim.keymap.set("n", "gb", "<cmd>b#<cr>")
+		vim.keymap.set("n", "]b", "<cmd>bn<cr>")
+		vim.keymap.set("n", "[b", "<cmd>bp<cr>")
+		vim.keymap.set("n", "]c", "<cmd>cn<cr>")
+		vim.keymap.set("n", "[c", "<cmd>cp<cr>")
+
+		-- plugins
+		vim.keymap.set("n", "<leader><space>", "<cmd>FzfLua files<cr>")
+		vim.keymap.set({ "n", "v" }, "<leader>ff", "<cmd>FzfLua<cr>")
+		vim.keymap.set("n", "<leader>fh", "<cmd>FzfLua helptags<cr>")
+		vim.keymap.set("n", "<leader>fj", "<cmd>FzfLua jumps<cr>")
+		vim.keymap.set("n", "<leader>fb", "<cmd>FzfLua buffers<cr>")
+		vim.keymap.set("n", "<leader>fz", "<cmd>FzfLua zoxide<cr>")
+		vim.keymap.set("n", "<leader>fg", "<cmd>FzfLua live_grep<cr>")
+		vim.keymap.set("n", "<leader>fl", "<cmd>FzfLua lines<cr>")
+		vim.keymap.set("n", "<leader>fd", "<cmd>FzfLua diagnostics_document<cr>")
+		vim.keymap.set("v", "<leader>fv", "<cmd>FzfLua grep_visual<cr>")
+
+		vim.keymap.set("n", "<leader>r", "<cmd>GrugFar<cr>", { desc = "Search and replace all files" })
+
+		vim.keymap.set("n", "<leader>gg", "<cmd>Neogit<cr>")
+		vim.keymap.set("n", "<leader>gd", "<cmd>DiffviewOpen<cr>")
+		vim.keymap.set("n", "<leader>gb", "<cmd>Neogit branch<cr>")
+		vim.keymap.set("n", "<leader>gl", "<cmd>Gitsigns toggle_current_line_blame<cr>")
+		vim.keymap.set("n", "<leader>gL", "<cmd>Gitsigns blame<cr>")
+		vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk_inline<cr>")
+		vim.keymap.set("n", "<leader>gr", "<cmd>Gitsigns reset_hunk<cr>")
+
+		vim.keymap.set("n", "<leader>n", "<cmd>Namu symbols<cr>", { desc = "Open LSP search" })
+
+		vim.keymap.set("n", "<bs>", "<cmd>NoiceDismiss<cr>")
+	end
+
+	KEYMAPS.mini_files = function(mini_files)
+		mini_files.config.mappings.close = "<esc>"
+		vim.keymap.set("n", "-", function()
+			mini_files.open(vim.fn.expand("%:p:h"), false)
+		end, { desc = "Open file explorer" })
+		vim.keymap.set("n", "<leader>-", function()
+			local state = mini_files.get_explorer_state()
+			local dir = state and state.branch[state.depth_focus] or "%:h"
+			vim.cmd("cd " .. dir)
+			vim.cmd("pwd")
+		end, { desc = "Set working directory here" })
+	end
+
+	KEYMAPS.lsp = function(event)
+		local opts = { buffer = event.buf }
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = event.buf, desc = "Show LSP hover at cursor" })
+		vim.keymap.set("n", "gd", "<cmd>FzfLua lsp_definitions<cr>", opts)
+		vim.keymap.set("n", "gi", "<cmd>FzfLua lsp_implementations<cr>", opts)
+		vim.keymap.set("n", "go", "<cmd>FzfLua lsp_type_defs<cr>", opts)
+		vim.keymap.set("n", "gr", "<cmd>FzfLua lsp_references<cr>", opts)
+		vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, { buffer = event.buf, desc = "Go to signature help" })
+		vim.keymap.set("n", "<leader>2", vim.lsp.buf.rename, { buffer = event.buf, desc = "Rename reference" })
+		vim.keymap.set({ "n", "x" }, "<leader>3", vim.lsp.buf.format, { buffer = event.buf, desc = "Format file" })
+		vim.keymap.set(
+			"n",
+			"<leader>4",
+			"<cmd>FzfLua lsp_code_actions<cr>",
+			{ buffer = event.buf, desc = "Show code actions" }
+		)
+	end
+
+	local has_words_before = function()
+		local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+		return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+	end
+
+	KEYMAPS.cmp = {
+		preset = "none",
+		["<cr>"] = { "accept", "fallback" },
+		["<tab>"] = {
+			function(cmp)
+				if has_words_before() and not cmp.is_visible() then
+					return cmp.show() and cmp.select_next()
+				elseif cmp.is_visible() then
+					return cmp.select_next()
+				end
+				return nil
+			end,
+		},
+		["<S-tab>"] = { "show", "select_prev", "fallback" },
+		["<C-j>"] = { "scroll_documentation_down" },
+		["<C-k>"] = { "scroll_documentation_up" },
+	}
+end
+
+KEYMAPS.general()
+
 local PLUGINS = {
 	{ "folke/tokyonight.nvim", opts = { transparent = true, styles = { floats = "transparent" } } },
 	{
@@ -54,8 +177,111 @@ local PLUGINS = {
 		end,
 	},
 	{ "ThePrimeagen/vim-be-good" },
-	{ "nvim-treesitter/nvim-treesitter" },
-	{ "neovim/nvim-lspconfig" },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"cpp",
+					"typescript",
+					"tsx",
+					"python",
+					"luau",
+					"javascript",
+					"rust",
+					"json",
+					"lua",
+					"latex",
+				},
+				highlight = {
+					enable = true,
+					disable = function(lang, buf)
+						if lang == "latex" then
+							return true
+						end
+						local max_filesize = 100 * 1024
+						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+						return ok and stats and stats.size > max_filesize
+					end,
+				},
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						node_incremental = "v",
+						node_decremental = "V",
+					},
+				},
+				textobjects = {
+					select = {
+						enable = true,
+						-- Automatically jump forward to textobj, similar to targets.vim
+						lookahead = true,
+						keymaps = {
+							-- You can use the capture groups defined in textobjects.scm
+							["af"] = { query = "@function.outer", desc = "function" },
+							["if"] = { query = "@function.inner", desc = "inner function" },
+							["ac"] = { query = "@class.outer", desc = "class" },
+							-- You can optionally set descriptions to the mappings (used in the desc parameter of
+							-- nvim_buf_set_keymap) which plugins like which-key display
+							["ic"] = { query = "@class.inner", desc = "inner class" },
+							-- You can also use captures from other query groups like `locals.scm`
+							["al"] = { query = "@local.scope", query_group = "locals", desc = "scope" },
+						},
+					},
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			local default_config = {
+				capabilities = require("blink.cmp").get_lsp_capabilities(),
+				flags = { debounce_text_changes = 1000 },
+				-- on_init = function(client, _)
+				-- 	client.server_capabilities.semanticTokensProvider = nil
+				-- end,
+			}
+			local custom_config = {
+				luau_lsp = {
+					cmd = {
+						"luau-lsp",
+						"lsp",
+						"--definitions=~/roblox/globalTypes.d.luau",
+						"--docs=~/roblox/en-us.json",
+					},
+				},
+				lua_ls = { settings = { Lua = { diagnostics = { globals = { "vim" } } } } },
+				clangd = {},
+				pyright = {},
+				vtsls = {},
+				jsonls = {},
+				rust_analyzer = {},
+				texlab = {},
+			}
+
+			for lsp, config in pairs(custom_config) do
+				for k, v in pairs(default_config) do
+					if not config[k] then
+						config[k] = v
+					end
+				end
+				lspconfig[lsp].setup(config)
+			end
+
+			vim.api.nvim_create_autocmd("LspAttach", {
+				desc = "LSP actions",
+				callback = function(event)
+					KEYMAPS.lsp(event)
+				end,
+			})
+
+			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+			vim.lsp.handlers["textDocument/signatureHelp"] =
+				vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+		end,
+	},
 	{ "williamboman/mason.nvim", opts = { ui = { border = "rounded" } } },
 	{ "williamboman/mason-lspconfig.nvim", opts = {
 		automatic_installation = true,
@@ -102,7 +328,21 @@ local PLUGINS = {
 			},
 		},
 	},
-	{ "echasnovski/mini.files" },
+	{
+		"echasnovski/mini.files",
+		config = function()
+			local mini_files = require("mini.files")
+			mini_files.setup({
+				windows = {
+					preview = true,
+					width_preview = 30,
+					width_nofocus = 30,
+					width_focus = 30,
+				},
+			})
+			KEYMAPS.mini_files(mini_files)
+		end,
+	},
 	{
 		"saghen/blink.cmp",
 		version = "*",
@@ -132,24 +372,7 @@ local PLUGINS = {
 				enabled = true,
 				window = { border = "rounded" },
 			},
-			keymap = {
-				preset = "none",
-				["<cr>"] = { "accept", "fallback" },
-				["<tab>"] = {
-					function(cmp)
-						if has_words_before() and not cmp.is_visible() then
-							return cmp.show() and cmp.select_next()
-						elseif cmp.is_visible() then
-							return cmp.select_next()
-						end
-						return nil
-					end,
-					"fallback",
-				},
-				["<S-tab>"] = { "show", "select_prev", "fallback" },
-				["<C-j>"] = { "scroll_documentation_down" },
-				["<C-k>"] = { "scroll_documentation_up" },
-			},
+			keymap = KEYMAPS.cmp,
 			sources = {
 				default = {
 					"lazydev",
@@ -267,109 +490,6 @@ local PLUGINS = {
 	},
 	{ "norcalli/nvim-colorizer.lua", opts = {} },
 }
-
-local KEYMAPS = {}
-do
-	vim.g.mapleader = " "
-	vim.g.maplocalleader = " "
-	local set = vim.keymap.set
-
-	KEYMAPS.general = function()
-		-- editor
-		set("i", "{<cr>", "{<cr>}<esc>O")
-		set("i", "{<s-cr>", "{<cr>}<esc>O")
-		set("v", "<leader>y", '"+y', { desc = "Copy selected to clipboard" })
-		set("n", "<leader>y", "<cmd>%y+<cr>", { desc = "Copy entire file contents to clipboard" })
-		set("n", "<leader>Y", function()
-			vim.fn.setreg("+", vim.fn.expand("%:p"))
-		end, { desc = "Copy file path to clipboard" })
-		set({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste clipboard" })
-		set({ "n", "v" }, "<leader>P", '"+P', { desc = "Paste clipboard" })
-		set("n", "<leader>m", "<cmd>e $MYVIMRC<cr>", { desc = "Edit vim config" })
-		set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show diagnostics at cursor" })
-		set("n", "<leader>w", "<cmd>tabc<cr>", { desc = "Close tab" })
-
-		-- competitive programming
-		set("n", "<leader>t", function()
-			vim.cmd("vsplit +term\\ ./run\\ " .. vim.fn.expand("%:r"))
-			vim.cmd("startinsert!")
-		end, { desc = "Run program in terminal split" })
-		set("t", "<esc>", "<cmd>bd!<cr>")
-		set("t", "<c-n>", "<c-\\><c-n>")
-
-		-- fast navigation
-		set("n", "<leader>h", "<c-w>h", { desc = "Move to left pane" })
-		set("n", "<leader>j", "<c-w>j", { desc = "Move to lower pane" })
-		set("n", "<leader>k", "<c-w>k", { desc = "Move to upper pane" })
-		set("n", "<leader>l", "<c-w>l", { desc = "Move to right pane" })
-		set("n", "gb", "<cmd>b#<cr>")
-		set("n", "]b", "<cmd>bn<cr>")
-		set("n", "[b", "<cmd>bp<cr>")
-		set("n", "]c", "<cmd>cn<cr>")
-		set("n", "[c", "<cmd>cp<cr>")
-
-		-- plugins
-		set("n", "<leader><space>", "<cmd>FzfLua files<cr>")
-		set({ "n", "v" }, "<leader>ff", "<cmd>FzfLua<cr>")
-		set("n", "<leader>fh", "<cmd>FzfLua helptags<cr>")
-		set("n", "<leader>fj", "<cmd>FzfLua jumps<cr>")
-		set("n", "<leader>fb", "<cmd>FzfLua buffers<cr>")
-		set("n", "<leader>fz", "<cmd>FzfLua zoxide<cr>")
-		set("n", "<leader>fg", "<cmd>FzfLua live_grep<cr>")
-		set("n", "<leader>fl", "<cmd>FzfLua lines<cr>")
-		set("n", "<leader>fd", "<cmd>FzfLua diagnostics_document<cr>")
-		set("v", "<leader>fv", "<cmd>FzfLua grep_visual<cr>")
-
-		set("n", "<leader>r", "<cmd>GrugFar<cr>", { desc = "Search and replace all files" })
-
-		set("n", "<leader>gg", "<cmd>Neogit<cr>")
-		set("n", "<leader>gd", "<cmd>DiffviewOpen<cr>")
-		set("n", "<leader>gb", "<cmd>Neogit branch<cr>")
-		set("n", "<leader>gl", "<cmd>Gitsigns toggle_current_line_blame<cr>")
-		set("n", "<leader>gL", "<cmd>Gitsigns blame<cr>")
-		set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk_inline<cr>")
-		set("n", "<leader>gr", "<cmd>Gitsigns reset_hunk<cr>")
-
-		set("n", "<leader>n", "<cmd>Namu symbols<cr>", { desc = "Open LSP search" })
-
-		set("n", "<bs>", "<cmd>NoiceDismiss<cr>")
-	end
-
-	KEYMAPS.mini_files = function(mini_files)
-		mini_files.config.mappings.close = "<esc>"
-		set("n", "-", function()
-			mini_files.open(vim.fn.expand("%:p:h"), false)
-		end, { desc = "Open file explorer" })
-		set("n", "<leader>-", function()
-			local state = mini_files.get_explorer_state()
-			local dir = state and state.branch[state.depth_focus] or "%:h"
-			vim.cmd("cd " .. dir)
-			vim.cmd("pwd")
-		end, { desc = "Set working directory here" })
-	end
-
-	KEYMAPS.lsp = function(event)
-		local opts = { buffer = event.buf }
-		set("n", "K", vim.lsp.buf.hover, { buffer = event.buf, desc = "Show LSP hover at cursor" })
-		set("n", "gd", "<cmd>FzfLua lsp_definitions<cr>", opts)
-		set("n", "gi", "<cmd>FzfLua lsp_implementations<cr>", opts)
-		set("n", "go", "<cmd>FzfLua lsp_type_defs<cr>", opts)
-		set("n", "gr", "<cmd>FzfLua lsp_references<cr>", opts)
-		set("n", "gs", vim.lsp.buf.signature_help, { buffer = event.buf, desc = "Go to signature help" })
-		set("n", "<leader>2", vim.lsp.buf.rename, { buffer = event.buf, desc = "Rename reference" })
-		set({ "n", "x" }, "<leader>3", vim.lsp.buf.format, { buffer = event.buf, desc = "Format file" })
-		set("n", "<leader>4", "<cmd>FzfLua lsp_code_actions<cr>", { buffer = event.buf, desc = "Show code actions" })
-	end
-
-	local has_words_before = function()
-		local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-		return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-	end
-
-	-- KEYMAPS.cmp =
-end
-
-KEYMAPS.general()
 
 -----------------
 -- VIM OPTIONS --
@@ -556,45 +676,6 @@ vim.cmd("colorscheme rose-pine")
 
 -- require("mason").setup()
 -- require("mason-lspconfig").setup()
-local lspconfig = require("lspconfig")
-local default_config = {
-	capabilities = require("blink.cmp").get_lsp_capabilities(),
-	flags = { debounce_text_changes = 1000 },
-	-- on_init = function(client, _)
-	-- 	client.server_capabilities.semanticTokensProvider = nil
-	-- end,
-}
-local custom_config = {
-	luau_lsp = {
-		cmd = { "luau-lsp", "lsp", "--definitions=~/roblox/globalTypes.d.luau", "--docs=~/roblox/en-us.json" },
-	},
-	lua_ls = { settings = { Lua = { diagnostics = { globals = { "vim" } } } } },
-	clangd = {},
-	pyright = {},
-	vtsls = {},
-	jsonls = {},
-	rust_analyzer = {},
-	texlab = {},
-}
-
-for lsp, config in pairs(custom_config) do
-	for k, v in pairs(default_config) do
-		if not config[k] then
-			config[k] = v
-		end
-	end
-	lspconfig[lsp].setup(config)
-end
-
-vim.api.nvim_create_autocmd("LspAttach", {
-	desc = "LSP actions",
-	callback = function(event)
-		KEYMAPS.lsp(event)
-	end,
-})
-
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
 
 ------------------
 -- AUTOCOMPLETE --
@@ -607,57 +688,6 @@ vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.s
 -- TREESITTER --
 ----------------
 
-require("nvim-treesitter.configs").setup({
-	ensure_installed = { "cpp", "typescript", "tsx", "python", "luau", "javascript", "rust", "json", "lua", "latex" },
-	highlight = {
-		enable = true,
-		disable = function(lang, buf)
-			if lang == "latex" then
-				return true
-			end
-			local max_filesize = 100 * 1024
-			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-			return ok and stats and stats.size > max_filesize
-		end,
-	},
-	incremental_selection = {
-		enable = true,
-		keymaps = {
-			node_incremental = "v",
-			node_decremental = "V",
-		},
-	},
-	textobjects = {
-		select = {
-			enable = true,
-			-- Automatically jump forward to textobj, similar to targets.vim
-			lookahead = true,
-			keymaps = {
-				-- You can use the capture groups defined in textobjects.scm
-				["af"] = { query = "@function.outer", desc = "function" },
-				["if"] = { query = "@function.inner", desc = "inner function" },
-				["ac"] = { query = "@class.outer", desc = "class" },
-				-- You can optionally set descriptions to the mappings (used in the desc parameter of
-				-- nvim_buf_set_keymap) which plugins like which-key display
-				["ic"] = { query = "@class.inner", desc = "inner class" },
-				-- You can also use captures from other query groups like `locals.scm`
-				["al"] = { query = "@local.scope", query_group = "locals", desc = "scope" },
-			},
-		},
-	},
-})
-
 ----------------
 -- MINI.FILES --
 ----------------
-
-local mini_files = require("mini.files")
-mini_files.setup({
-	windows = {
-		preview = true,
-		width_preview = 30,
-		width_nofocus = 30,
-		width_focus = 30,
-	},
-})
-KEYMAPS.mini_files(mini_files)
