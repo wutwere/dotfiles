abec0a2efbaf873fe887e13effb6222705869eab
wutwere <62412610+wutwere@users.noreply.github.com>
2025-04-04 00:32:50 -0400

    update treesitter, colors, font

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -5,8 +5,8 @@ return {
 		name = "rose-pine",
 		priority = 1000,
 		opts = {
-			variant = "moon", -- auto, main, moon, or dawn
-			dark_variant = "moon", -- main, moon, or dawn
+			variant = "auto", -- auto, main, moon, or dawn
+			dark_variant = "main", -- main, moon, or dawn
 			dim_inactive_windows = false,
 			extend_background_behind_borders = true,
 
