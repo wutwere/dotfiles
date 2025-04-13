c4c35b2061672c5678764d959a408edad0895f1a
wutwere <62412610+wutwere@users.noreply.github.com>
2025-04-13 02:25:28 -0400

    disable ai when file too big

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -33,8 +33,8 @@ return {
 							return true
 						end
 						local max_filesize = 100 * 1024
-						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-						return ok and stats and stats.size > max_filesize
+						local ok, size = pcall(vim.fn.getfsize, vim.api.nvim_buf_get_name(buf))
+						return ok and (size == -2 or size > max_filesize)
 					end,
 				},
 				incremental_selection = {
