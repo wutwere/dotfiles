6dd1b30cb0b7f83413df4abaa6e857a8ae35ba2e
wutwere <62412610+wutwere@users.noreply.github.com>
2025-08-09 18:44:29 -0700

    fix wsl paste

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,6 +5,22 @@ KEYMAPS.general()
 -- VIM OPTIONS --
 -----------------
 
+if vim.fn.has("wsl") == 1 then
+	vim.g.clipboard = {
+		name = "WslClipboard",
+		copy = {
+			["+"] = "clip.exe",
+			["*"] = "clip.exe",
+		},
+		paste = {
+			-- remember scoop install win32yank
+			["+"] = "win32yank.exe -o --lf",
+			["*"] = "win32yank.exe -o --lf",
+		},
+		cache_enabled = 0,
+	}
+end
+
 vim.g.netrw_bufsettings = "noma nomod nu rnu nobl nowrap ro"
 vim.opt.autoindent = true
 vim.opt.breakindent = true
